873179f5a559f8ddf1706e3eb66aa79d24a9b1a6
Aida Abdikulova <abdikulova@gmail.com>
2020-02-29 07:05:21 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -1,4 +1,23 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  configure do
+    enable :sessions
+    set :session_secret, "bubba"
+  end
+
+  get '/' do
+    erb :index
+  end
+
+  post '/checkout' do
+    @item = params[:item]
+    @session[@item]
+    @session = session
+    binding.pry
+
+    erb :new
+  end
+
 end
